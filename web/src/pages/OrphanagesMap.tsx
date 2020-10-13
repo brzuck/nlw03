112b97cb4a8ddf5009e760fbb6fe56cca5efd994
Bruno Barros <bruno@cubeti.com.br>
2020-10-12 22:29:53 -0300

    creating orphanages map page

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,7 +1,43 @@
+import 'leaflet/dist/leaflet.css';
 import React from 'react';
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+import { Link } from 'react-router-dom';
+import mapMarketImg from '../images/map-marker.svg';
+import '../styles/pages/orphanages-map.css';
 
 function OrphanagesMap() {
-  return <h1>Hello World</h1>;
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarketImg} alt="Happy" />
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Vila Velha</strong>
+          <span>Espírito Santo</span>
+        </footer>
+      </aside>
+
+      <Map
+        center={[-20.37018, -40.2986415]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}>
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+      </Map>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#fff" />
+      </Link>
+    </div>
+  );
 }
 
 export default OrphanagesMap;
